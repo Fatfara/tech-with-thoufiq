979580377ad6843443c487b680a928d4f7d9aa4d
Fatfara <thoufiqabubacker75@gmail.com>
2026-06-29 14:13:24 +0530

    Add core configuration layer

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -6,7 +6,9 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+from app.core.config import settings
+
+DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
